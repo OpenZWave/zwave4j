ffc73b4474280c3aa1bc24e71b7b3bc57bd52007
Alexander Zagumennikov <zgmnkv@users.noreply.github.com>
2015-11-07 22:56:44 +0400

    Change license to MIT

--- src/main/java/org/zwave4j/NativeLibraryLoader.java
+++ src/main/java/org/zwave4j/NativeLibraryLoader.java
@@ -1,24 +1,3 @@
-/*
- * Copyright (c) 2013 Alexander Zagumennikov
- *
- * SOFTWARE NOTICE AND LICENSE
- *
- * This file is part of ZWave4J.
- *
- * ZWave4J is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * ZWave4J is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with ZWave4J.  If not, see <http://www.gnu.org/licenses/>.
- */
-
 package org.zwave4j;
 
 import java.io.*;
